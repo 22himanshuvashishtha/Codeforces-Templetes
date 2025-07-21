ea08a7ca4b45f2ec0c7e5c7b1ac214bb3562f199
22himanshuvashishtha <149816813+22himanshuvashishtha@users.noreply.github.com>
2025-07-21 17:23:05 +0530

    Create Prime_Factors.cpp

---
+++ Prime_Factors.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<int>fact;
+    for(int i=2;i*i<=n;i++){
+      if(n%i==0){
+        fact.push_back(i);
+        n/=i;
+      }
+    }
+    if(n>1){
+      fact.push_back(n);
+    }
+    for(auto it:fact){
+      cout<<it<<" ";
+    }
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    //int t;
+    //cin >> t;
+    //while (t--) 
+    solve();
+
+    return 0;
+}
