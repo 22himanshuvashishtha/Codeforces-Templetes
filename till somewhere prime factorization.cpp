318d62251a785b43bf749ce44b66d1586353e919
22himanshuvashishtha <149816813+22himanshuvashishtha@users.noreply.github.com>
2025-07-22 09:00:26 +0530

    Create till somewhere prime factorization.cpp

---
+++ till somewhere prime factorization.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<bool>prime(n+1,true);
+    prime[0]=prime[1]=false;
+    for(int i=2;i<=n;i++){
+      if(prime[i]){
+        for(int j=i*i;j<=n;j+=i){
+          prime[j]=false;
+        }
+      }
+    }
+    for(auto it:prime){
+      cout<<it<<" ";
+    }
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    //int t;
+    //cin >> t;
+    //while (t--) 
+    solve();
+
+    return 0;
+}
